9ab148e4e06319a79646dcf30e872fe306d8f5a8
ian <iannpanait@gmail.com>
2024-04-26 17:21:47 -0700

    Created Vision Subsystem and VisionData class. Also added SetTarget and GetData methods to vision.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Constants {
+    public static class VisionConstants {
+        public static int BackCameraWidth = 1280;
+        public static int BackCameraHeight = 720;
+    }
+
+}
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsytems/Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsytems/Vision.java
@@ -1,40 +1,61 @@
 package org.firstinspires.ftc.teamcode.subsytems;
 
+import androidx.annotation.NonNull;
+
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.arcrobotics.ftclib.geometry.Pose2d;
+import com.arcrobotics.ftclib.vision.AprilTagDetector;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.Constants;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
 import org.firstinspires.ftc.vision.apriltag.AprilTagLibrary;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.openftc.apriltag.AprilTagDetectorJNI;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.tensorflow.lite.task.vision.detector.Detection;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Optional;
+
 
 public class Vision extends SubsystemBase {
-    private AprilTagProcessor m_processor;
-    private VisionPortal m_visionPortal;
+   private AprilTagDetector m_detector;
+   private Mode m_mode;
 
-    public Vision(HardwareMap hardwareMap, String cameraName) {
-        m_processor = new AprilTagProcessor.Builder()
-                .setTagFamily(AprilTagProcessor.TagFamily.TAG_36h11)
-                .setTagLibrary(AprilTagGameDatabase.getCenterStageTagLibrary())
-                .setDrawTagID(true)
-                .setOutputUnits(DistanceUnit.METER, AngleUnit.DEGREES)
-                .build();
-        m_visionPortal = new VisionPortal.Builder()
-                .addProcessors(m_processor)
-                .setCamera(hardwareMap.get(WebcamName.class, "frontCamera"))
-                .enableLiveView(true)
-                .setAutoStartStreamOnBuild(true)
-                .build();
+    public enum Mode {
+        RED_BACKDROP,
+        BLUE_BACKDROP,
+        LOCALIZATION,
+        STACK
     }
 
-    public int seenID() {
-        if(!m_processor.getDetections().isEmpty()) {
-            return m_processor.getDetections().get(0).id;
+    public Vision(HardwareMap hardwareMap) {
+        m_detector = new AprilTagDetector(hardwareMap);
+        m_detector.WIDTH = Constants.VisionConstants.BackCameraWidth; // In Constants file
+        m_detector.HEIGHT = Constants.VisionConstants.BackCameraHeight;
+        m_detector.ORIENTATION = OpenCvCameraRotation.UPRIGHT;
+        // m_detector.GPU_ENABLED = true; TODO Determine whether gpu enabled is good
+        m_detector.init();
+
+
+    }
+    public Optional<VisionData> getData () {
+        // sending Data to VisionData class
+        Map<String, Integer> detection = m_detector.getDetection();
+        if(detection != null) {
+            return Optional.of(new VisionData(detection.get("x"), detection.get("y"), detection.get("id")));
         }
-        return -1;
+        return Optional.empty();
+    }
+    public void setTarget(@NonNull Integer... ids) {
+        m_detector.setTargets(ids);
     }
 }
+
+
