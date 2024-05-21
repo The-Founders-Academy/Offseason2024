344fc2554c9011d66fdafa41906e6e3d58346d85
State Of Mind <robotics@tfanh.org>
2024-05-21 13:52:29 -0400

    Added an opmode for testing rotations

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing/auto/AutoRotate.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.opmodes.testing.auto;
+
+import com.acmerobotics.dashboard.canvas.Rotation;
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.CommandScheduler;
+import com.arcrobotics.ftclib.geometry.Pose2d;
+import com.arcrobotics.ftclib.geometry.Rotation2d;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+
+import org.firstinspires.ftc.teamcode.commands.DriveToPosition;
+import org.firstinspires.ftc.teamcode.mecanum.MecanumConfigs;
+import org.firstinspires.ftc.teamcode.subsytems.Mecanum2024;
+
+public class AutoRotate extends CommandOpMode {
+    private Mecanum2024 m_mecanumDrive;
+    private Rotation2d m_intiialRotation = new Rotation2d(0);
+    private Rotation2d m_targetRotation = new Rotation2d(Math.PI);
+    @Override
+    public void initialize() {
+        MecanumConfigs configs = new MecanumConfigs().runMode(Motor.RunMode.RawPower);
+        m_mecanumDrive = new Mecanum2024(hardwareMap, configs, new Pose2d(0, 0, m_intiialRotation));
+
+        DriveToPosition rotate = new DriveToPosition(m_mecanumDrive, m_mecanumDrive.getPose());
+        CommandScheduler.getInstance().schedule(rotate.withTimeout(2000));
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing/teleop/DriveTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing/teleop/DriveTest.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.opmodes.testing.teleop;
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.geometry.Pose2d;
 import com.arcrobotics.ftclib.geometry.Rotation2d;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.commands.DriverRelativeDrive;
@@ -16,8 +17,9 @@ public class DriveTest extends CommandOpMode {
     private Mecanum2024 m_mecanumDrive;
     @Override
     public void initialize() {
+        MecanumConfigs configs = new MecanumConfigs().runMode(Motor.RunMode.RawPower);
+        m_mecanumDrive = new Mecanum2024(hardwareMap, configs, new Pose2d(0, 0, Rotation2d.fromDegrees(0)));
         m_driver = new CommandGamepad(gamepad1, 1, 1);
-        m_mecanumDrive = new Mecanum2024(hardwareMap, new MecanumConfigs(), new Pose2d(0, 0, Rotation2d.fromDegrees(0)));
         m_mecanumDrive.setDefaultCommand(new DriverRelativeDrive(m_mecanumDrive, m_driver));
     }
 }
