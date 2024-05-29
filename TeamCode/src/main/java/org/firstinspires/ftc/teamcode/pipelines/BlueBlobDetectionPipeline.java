c98bb02c8bac0a57bbd650c0948bbe2cb312e072
ian <iannpanait@gmail.com>
2024-05-29 19:00:41 -0400

    Initial branch commit, changed location of RedBlob and BlueBlob detection files, and changed it from returning strings to returning ENUMS.
    !! Will have to test enum return values to make sure they're right !!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pipelines/BlueBlobDetectionPipeline.java
@@ -0,0 +1,171 @@
+package org.firstinspires.ftc.teamcode.pipelines;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Point;
+import org.opencv.core.MatOfPoint;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+import java.util.ArrayList;
+import java.util.List;
+public class BlueBlobDetectionPipeline extends OpenCvPipeline {
+
+    public enum BluePropZone  {
+        LEFT, CENTER, RIGHT
+    }
+
+    double cX = 0;
+    double cY = 0;
+
+    // Width of Camera || TODO Replace with actual value
+    double width = 0;
+
+    // These values are for the back camera, if we get a front camera remember to check specific values for that
+    private static final int cameraWidth = 1280; // Values for EOCV-Sim, actual value is 1280, EOCV-Sim is 640
+    private static final int cameraHeight = 720; // Values for EOCV-Sim, actual value is 720, EOCV-Sim is 480
+
+    // NOT CORRECT NUMBERS
+    public static final double realUnitObjectWidth = 3.75; // TODO Replace with correct width of object in real units.
+    public static final double focalLength = 728; // TODO Replace with correct Focal Length of Camera
+
+
+    @Override
+    public Mat processFrame(Mat input) {
+        int top = input.rows() / 2; // Top boundary of the ROI (halfway down the image)
+
+
+        // Preprocesses the frame to detect yellow areas
+        Mat yellowMask = preprocessFrame(input);
+
+        // Finds contours (areas) of the yellow regions it detected
+        List<MatOfPoint> contours = new ArrayList<>();
+        Mat hierarchy = new Mat();
+        Imgproc.findContours(yellowMask, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        /* Finds the largest yellow contour and assigns it to "largestContour",
+           using the findLargestContour method which is written below */
+        MatOfPoint largestContour = findLargestContour(contours);
+
+        if (largestContour != null) {
+            // Draw a red outline around the largest detected object
+            Imgproc.drawContours(input, contours, contours.indexOf(largestContour), new Scalar(0, 255, 0), 2);
+
+            // Calculate the width of the bounding box, using the calculateWidth method which is written below
+            width = calculateWidth(largestContour);
+
+            // Display the width next to the label
+            String widthLabel = "Width: " + (int) width + " pixels";
+            Imgproc.putText(input, widthLabel, new Point(cX + 10, cY + 20), Imgproc.FONT_HERSHEY_SIMPLEX, 0.5, new Scalar(0, 255, 0), 2);
+
+            // Display the Distance
+            String distanceLabel = "Distance: " + String.format("%.2f", getDistance(width)) + " inches";
+            Imgproc.putText(input, distanceLabel, new Point(cX + 10, cY + 60), Imgproc.FONT_HERSHEY_SIMPLEX, 0.5, new Scalar(0, 255, 0), 2);
+
+            // Calculate the centroid (center point) of the largest contour
+            Point centroid = calculateCentroid(largestContour);
+            cX = centroid.x;
+            cY = centroid.y;
+
+            // Draw a dot at the centroid
+            String label = "(" + (int) cX + ", " + (int) cY + ")";
+            Imgproc.putText(input, label, new Point(cX + 10, cY), Imgproc.FONT_HERSHEY_COMPLEX, 0.5, new Scalar(0, 255, 0), 2);
+            Imgproc.circle(input, new Point(cX, cY), 5, new Scalar(0, 255, 0), -1);
+
+            // Say what third it's in
+            BluePropZone BlueZone = GetBluePropZone(cX);
+        }
+
+        return input;
+    }
+
+    // Method for preprocessing Frames, which is called in YellowBlobDetectionPipeline function.
+    private Mat preprocessFrame(Mat frame) {
+        Mat hsvFrame = new Mat();
+        Imgproc.cvtColor(frame, hsvFrame, Imgproc.COLOR_BGR2HSV);
+
+        // Scalars contain HSV values, values between 100 and 180 contain the values that red is in.
+        // TODO may need to change the values to something else
+        Scalar lowerYellow = new Scalar(0, 100, 100);
+        Scalar upperYellow = new Scalar(60, 255, 255);
+
+        Mat yellowMask = new Mat();
+        Core.inRange(hsvFrame, lowerYellow, upperYellow, yellowMask);
+
+        Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new org.opencv.core.Size(5, 5));
+        Imgproc.morphologyEx(yellowMask, yellowMask, Imgproc.MORPH_OPEN, kernel);
+        Imgproc.morphologyEx(yellowMask, yellowMask, Imgproc.MORPH_CLOSE, kernel);
+
+        return yellowMask;
+    }
+
+    // Method for finding the largest area of yellow, used in the function YellowBlobDetectionPipeline function.
+    private MatOfPoint findLargestContour(List<MatOfPoint> contours) {
+        double maxArea = 0;
+        MatOfPoint largestContour = null;
+
+        for (MatOfPoint contour : contours) {
+            double area = Imgproc.contourArea(contour);
+            if (area > maxArea) {
+                maxArea = area;
+                largestContour = contour;
+            }
+        }
+
+        return largestContour;
+    }
+
+    // Method used to calculate the width of bounding box, (outline being drawn along the largest blob)
+    private double calculateWidth(MatOfPoint contour) {
+        Rect boundingRect = Imgproc.boundingRect(contour);
+        return boundingRect.width;
+    }
+
+
+    // Method to calculate the centroid of a contour
+    private Point calculateCentroid(MatOfPoint contour) {
+        Point centroid = new Point(0, 0);
+        int numPoints = contour.rows();
+
+        for (int i = 0; i < numPoints; i++) {
+            double[] point = contour.get(i, 0);
+            centroid.x += point[0];
+            centroid.y += point[1];
+        }
+
+        centroid.x /= numPoints;
+        centroid.y /= numPoints;
+
+        return centroid;
+    }
+    private BluePropZone GetBluePropZone(double cord) {
+        double coordinate = cord;
+        double left_third = cameraWidth * 0.33;
+        double center_third = cameraWidth * 0.66;
+
+        BluePropZone BlueZone = BluePropZone.CENTER; // Sets initial value of BlueZone to Center
+
+
+
+        if (coordinate <= left_third) {
+
+           BlueZone = BluePropZone.LEFT;
+        }
+        else if (coordinate > left_third && coordinate <= center_third) {
+            BlueZone = BluePropZone.CENTER;
+        }
+        else {
+            BlueZone = BluePropZone.RIGHT;
+
+
+        }
+        return BlueZone;
+    }
+
+    // Used in the initial RunOpMode command to display live telemetry data, as well as displaying it as a label next to the contour box in the YellowBlobDetectionPipeline function.
+    private static double getDistance(double width) {
+        double distance = (realUnitObjectWidth * focalLength) / width; // THE FORMULA! (Again remember to change realUnitObjectWidth to correct width of team prop)
+        return distance;
+    }
+}
+
